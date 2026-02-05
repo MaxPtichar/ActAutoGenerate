7cb0c5f733c1473c80db135f2ab689aa46279574
MaxPtichar <dart546@gmail.com>
2026-02-05 18:55:23 +0300

    feat: added module main, create folder for output files, deleted unnecessary libs

--- src/doc_engine.py
+++ src/doc_engine.py
@@ -1,24 +1,39 @@
-from datetime import date
+from pathlib import Path
 
 from docxtpl import DocxTemplate
 
 from constants import MONTHS_RU
 from database import DBManager
-from models import Organization, Requisites
 
 db = DBManager("database/organization.db")
 
 
-all_fetch = db.fetch_organization()
-if all_fetch:
-    for org in all_fetch:
-        doc = DocxTemplate("template//template1.docx")
-        context = {"org": org}
-        print(org)
-        doc.render(context)
+def path_save(filename):
+    base_dir = Path(__file__).parent.parent
+    out_path = base_dir / "output"
+    out_path.mkdir(exist_ok=True)
+    return out_path / filename
 
-        doc.save(
-            f"{org.name} Акт за {MONTHS_RU[org.date.month][0]} № {org.act_counter}.docx"
-        )
-        org.prepare_next_period()
-        db.update_data(org)
+
+def create_files():
+    base_dir = Path(__file__).parent.parent
+    template_path = base_dir / "template" / "template1.docx"
+    if not template_path.exists():
+        print(f"ОШИБКА: Шаблон не найден по пути {template_path}")
+        return
+    all_fetch = db.fetch_organization()
+    if all_fetch:
+        try:
+            for org in all_fetch:
+                doc = DocxTemplate(str(template_path))
+                context = {"org": org}
+
+                doc.render(context)
+
+                filename = f"{org.name} Акт за {MONTHS_RU[org.date.month][0]} № {org.act_counter}.docx"
+                doc.save(path_save(filename))
+                print(f"Успех: {filename}")
+                org.prepare_next_period()
+                db.update_data(org)
+        except Exception as e:
+            print(f"Ошибка при обработке {org}: {e}")
---
+++ src/main.py
@@ -0,0 +1,4 @@
+import doc_engine
+
+if __name__ == "__main__":
+    doc_engine.create_files()
