894021342a6859d7141fc0e619d3ff1f17626c69
MaxPtichar <dart546@gmail.com>
2026-02-07 22:48:04 +0300

    feat: add class OrganizationTable

---
+++ src/UI/elements/organization_table.py
@@ -0,0 +1,48 @@
+from typing import Callable
+
+import flet as ft
+
+from src.models import Organization
+from src.services.organization_services import OrgServices
+
+
+class OrganizationTable(ft.Container):
+    def __init__(
+        self,
+        org_service: OrgServices,
+        on_edit: Callable[[Organization], None],
+        on_delete: Callable[[Organization], None],
+    ) -> None:
+        super().__init__()
+        self.org_service = org_service
+        self.on_edit = on_edit
+        self.on_delete = on_delete
+
+        self.content = self.build_columns()
+
+    def build_columns(self):
+        return ft.DataTable(
+            columns=[
+                ft.DataColumn(label=ft.Text("Название")),
+                ft.DataColumn(label=ft.Text("УНП")),
+                ft.DataColumn(label=ft.Text("Доход")),
+                ft.DataColumn(label=ft.Text("")),
+            ],
+            rows=self._build_rows(),
+        )
+
+    def _build_rows(self):
+        rows = []
+        for org in self.org_service.list_all():
+            rows.append(
+                ft.DataRow(
+                    cells=[
+                        ft.DataCell(ft.Text(org.name)),
+                        ft.DataCell(ft.Text(org.requisites.unp)),
+                        ft.DataCell(ft.Text(f"{int(org.fee)}")),
+                        ft.DataCell(ft.IconButton(ft.CupertinoIcons.ELLIPSIS_VERTICAL)),
+                    ]
+                )
+            )
+
+        return rows
