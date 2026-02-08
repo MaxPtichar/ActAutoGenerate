c9389fd730f741bc8a67c9be08392d0f67fa1f48
MaxPtichar <dart546@gmail.com>
2026-02-08 14:33:22 +0300

    feat: add _on_edit_org method and instant UI update for organizations

--- src/UI/views/organizations_view.py
+++ src/UI/views/organizations_view.py
@@ -3,6 +3,7 @@ from typing import Callable
 import flet as ft
 
 from src.services.organization_services import OrgServices
+from src.UI.components.OrgForm import OrgFormDialogAlert
 from src.UI.elements.organization_table import OrganizationTable
 
 
@@ -28,7 +29,20 @@ class ShowOrg(ft.Column):
         )
 
     def _on_edit_org(self, org: Organization):
-        print("Edit:", org.name)
+
+        def handle_save(update_org):
+            self.org.edit_org(update_org)
+            self.table.refresh_table()
+
+            self.page.snack_bar = ft.SnackBar(ft.Text("Данные обновлены"))
+            self.page.snack_bar.open = True
+
+            self.page.update()
+
+        dialog = OrgFormDialogAlert(org=org, on_save=handle_save)
+        self.table.refresh_table()
+        self.page.show_dialog(dialog)
+        self.page.update()
 
     def _on_delete_org(self, org: Organization):
         print("Delete:", org.name)
