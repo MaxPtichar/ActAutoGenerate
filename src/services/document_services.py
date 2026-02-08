d8ac02f1d64d6060095fc30729136765eb63aa28
MaxPtichar <dart546@gmail.com>
2026-02-08 18:15:11 +0300

    refactor: minor logic improvements and add path fetch method to doc services

--- src/services/document_services.py
+++ src/services/document_services.py
@@ -2,5 +2,12 @@ from src.core import doc_engine
 
 
 class DocumentService:
+    def __init__(self) -> None:
+        self.template_path: str | None = None
+
     def generate_acts(self) -> None:
-        doc_engine.create_files()
+        doc_engine.create_files(self.template_path)
+
+    def get_path_for_generate(self, path: str):
+
+        self.template_path = path
