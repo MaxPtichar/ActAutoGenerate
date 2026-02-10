dc92527a84a39a1bb763fdc4c8455d99d9a46d0e
MaxPtichar <dart546@gmail.com>
2026-02-10 20:47:39 +0300

    fix(org_validators): add return statements

--- src/validators/org_validators.py
+++ src/validators/org_validators.py
@@ -16,6 +16,8 @@ class OrgValidator:
         if not (2 <= len(name) <= 300):
             raise ValidationError("Длина строки должна быть от 2 до 300 символов")
 
+        return True
+
     @staticmethod
     def validate_manager_name(manager_name: str):
         if not all(ch.isalpha() or ch in " -." for ch in manager_name):
@@ -23,12 +25,16 @@ class OrgValidator:
         if not (2 <= len(manager_name) <= 300):
             raise ValidationError("Длина строки должна быть от 2 до 300 символов")
 
+        return True
+
     @staticmethod
     def validate_agreement(agreement: str):
         if not all(ch in ALLOWED_CHARS for ch in agreement):
             raise ValidationError("Номер договора содержит недопустимые символы")
         if not (2 <= len(agreement) <= 140):
             raise ValidationError("Длина строки должна быть от 2 до 140 символов")
+
+        return True
 
     @staticmethod
     def validate_fee(fee: str):
@@ -39,6 +45,8 @@ class OrgValidator:
         if not (0 < value < 1_000_000):
             raise ValidationError("Число не может быть меньше 0 и больше 1 000 000")
 
+        return True
+
     @staticmethod
     def validate_act_counter(act_counter: int):
         try:
@@ -48,12 +56,16 @@ class OrgValidator:
         if not (0 < value < 1_000):
             raise ValidationError("Число не может быть меньше 0 и больше 1 000 ")
 
+        return True
+
     @staticmethod
     def validate_date(date: str):  ## stop
         try:
             datetime.strptime(date, "%d.%m.%Y")
         except ValueError:
             raise ValidationError("Формат даты: ДД.ММ.ГГГГ")
+
+        return True
 
     @staticmethod
     def validate_unp(unp: str):
@@ -62,12 +74,16 @@ class OrgValidator:
         if len(unp) != 9:
             raise ValidationError("УНП должно содержать 9 цифр")
 
+        return True
+
     @staticmethod
     def validate_address(address: str):
         if not all(ch in ALLOWED_CHARS for ch in address):
             raise ValidationError("Адрес содержит недопустимые символы")
         if not (2 <= len(address) <= 300):
             raise ValidationError("Длина строки должна быть от 2 до 300 символов")
+
+        return True
 
     @staticmethod
     def validate_bank_account(bank_account: str):
@@ -76,12 +92,16 @@ class OrgValidator:
         if len(bank_account) != 28:
             raise ValidationError("Cчет должен быть в формате BY25MTBK + 20 цифр")
 
+        return True
+
     @staticmethod
     def validate_name_of_bank(name_of_bank: str):
-        if not all(ch in ALLOWED_CHARS for ch in name_of_bank):
+        if not all(ch.isalpha() or ch in ' -"' for ch in name_of_bank):
             raise ValidationError("Название содержит недопустимые символы")
         if not (2 <= len(name_of_bank) <= 50):
             raise ValidationError("Длина строки должна быть от 2 до 50 символов")
+
+        return True
 
     @staticmethod
     def validate_bic(bic: str):
@@ -90,6 +110,8 @@ class OrgValidator:
         if len(bic) != 8:
             raise ValidationError("BIC должен иметь 8 символов")
 
+        return True
+
     @staticmethod
     def validate_mobile_num(mobile_num: str):
         if not mobile_num.isdigit():
@@ -97,9 +119,13 @@ class OrgValidator:
         if not (6 <= len(mobile_num) <= 20):
             raise ValidationError("Номер телефона от 6 до 20 символов")
 
+        return True
+
     @staticmethod
     def validate_e_mail(e_mail: str):
         if "@" not in e_mail or "." not in e_mail:
             raise ValidationError("Неверный e-mail")
         if not (5 <= len(e_mail) <= 320):
             raise ValidationError("Номер телефона от 5 до 320 символов")
+
+        return True
